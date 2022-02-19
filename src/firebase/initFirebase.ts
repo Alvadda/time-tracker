54c2b5241205ea784995cb38bbe18fdb587950e7
Alvadda <58438582+Alvadda@users.noreply.github.com>
2022-02-19 15:25:40 +0100

    fix: cypress can run with firebase emulator

--- src/firebase/initFirebase.ts
+++ src/firebase/initFirebase.ts
@@ -1,10 +1,16 @@
 import { initializeApp } from 'firebase/app'
 import { connectAuthEmulator, getAuth } from 'firebase/auth'
-import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore'
+import { connectFirestoreEmulator, getFirestore, initializeFirestore } from 'firebase/firestore'
 import { firebaseConfig } from './firebase.config'
 
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore:next-line
+if (window.Cypress) {
+  initializeFirestore(app, { experimentalForceLongPolling: true })
+}
 const db = getFirestore(app)
 
 if (process.env.NODE_ENV !== 'production') {
