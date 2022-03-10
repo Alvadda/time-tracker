303d438725488953ca620fc8b458656163b1cb9c
Alvadda <58438582+Alvadda@users.noreply.github.com>
2022-03-10 09:15:09 +0100

    chore: create serviceaccount in pipeline

--- bin/createServiceAccount.js
+++ bin/createServiceAccount.js
@@ -4,6 +4,11 @@ const path = require('path')
 
 const serviceAccountString = process.env.FIREBASE_SERVICE_ACCOUNT
 
+if (!serviceAccountString) {
+  console.error('FIREBASE_SERVICE_ACCOUNT not found!')
+  process.exit(1)
+}
+
 if (serviceAccountString) {
   fs.writeFile(path.join(__dirname, '../', 'serviceAccount.json'), serviceAccountString, function (err) {
     if (err) return console.log(err)
