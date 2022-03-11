893f6236b27676b0dd7cf55a8bfdeeca0a7fa888
Alvadda <58438582+Alvadda@users.noreply.github.com>
2022-03-11 08:24:59 +0100

    test: only keep faild test vidoes

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,5 +1,6 @@
 import { plugin as cypressFirebasePlugin } from 'cypress-firebase'
 import admin from 'firebase-admin'
+const del = require('del')
 require('dotenv').config({ path: '.env' })
 
 module.exports = (on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) => {
@@ -9,6 +10,12 @@ module.exports = (on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions)
   config.env.FIREBASE_STORAGE_BUCKET = process.env.FIREBASE_STORAGE_BUCKET
   config.env.FIREBASE_MESSAGING_SENDER_ID = process.env.FIREBASE_MESSAGING_SENDER_ID
   config.env.FIREBASE_APP_ID = process.env.FIREBASE_APP_ID
+
+  on('after:spec', (_, results) => {
+    if (results.stats.failures === 0 && results.video) {
+      return del(results.video)
+    }
+  })
 
   const extendedConfig = cypressFirebasePlugin(on, config, admin)
   return extendedConfig
